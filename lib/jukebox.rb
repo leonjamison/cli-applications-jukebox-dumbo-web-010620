a6656f59f855a6802b290d1100af6b24f37df8f7
Leon Jamison <leonjamison@icloud.com>
2020-01-05 01:53:55 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,19 @@
 # Add your code here
+def help
+  :puts [play]
+  ]
+end
+#
+# def play
+#   :puts
+# end
+#
+# def list
+#   :puts
+# end
+#
+# def exit_jukebox
+#   :puts
+# end
+#
+# /I accept the following commands:/
